0c5329b1a0a7b3b8c88be2d04c4544aad24c361d
Ana Sabina Uban <ana.uban@gmail.com>
2017-11-22 16:24:51 +0200

    more links

--- pelicanconf.py
+++ pelicanconf.py
@@ -29,6 +29,7 @@ PROFILE_IMAGE = '/images/portrait_AnaUban.JPG'
 BACKDROP_IMAGE = '/images/public-domain-images-free-stock-photos-high-quality-resolution-downloads-around-the-house-7.jpg'
 SITE_DESCRIPTION= 'Software developer and researcher; Natural language processing and machine learning PhD student; Python software developer / freelancer; Enthusiastic about AI, education, language, ideas'
 EMAIL = 'ana.uban+prof@gmail.com'
+BLOGKEYWORDS = ('machine learning', 'natural language processing', 'python', 'NLP', 'ML', 'deep learning', 'freelance', 'remote', 'contractor')
 
 PAGINATED_DIRECT_TEMPLATES = ('categories', 'archives')
 
@@ -38,6 +39,7 @@ LINKS = (('Human Language Technologies Research Center', 'http://nlp.unibuc.ro')
 # Social widget
 SOCIAL = (('Linkedin', 'https://www.linkedin.com/in/ana-uban/'),
             ('UpWork', 'https://www.upwork.com/freelancers/~01f657838909aba2bf'),
+            ('AngelList','https://angel.co/ana-sabina-uban'),
             ('Academic', 'http://nlp.unibuc.ro/people/anauban.html')
           # ('Another social link', '#'),
           )
